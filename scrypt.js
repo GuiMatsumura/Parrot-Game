829c1ccb1a7aaa916e462e46192cb081a7f4f748
GuiMatsumura <guilhermepenamatsumura@gmail.com>
2024-08-07 00:36:37 -0300

    created the logic to create and shuffle a new parrot deck

--- scrypt.js
+++ scrypt.js
@@ -1,10 +1,22 @@
 const minCartas = 4;
 const maxCartas = 14;
+const todosPapagaios = [
+  "bobrossparrot",
+  "explodyparrot",
+  "fiestaparrot",
+  "metalparrot",
+  "revertitparrot",
+  "tripletsparrot",
+  "unicornparrot",
+];
 
 let qtdCartas = 0;
 let qtdJogadas = 0;
+let numDuplas = 0;
+let novoBaralho = [];
 
 obterQtdCartas();
+criarBaralho();
 criarCartas();
 
 function obterQtdCartas() {
@@ -21,10 +33,37 @@ function validarQtdCartas() {
   return par && qtdValida;
 }
 
+function criarBaralho() {
+  numDuplas = qtdCartas / 2;
+
+  let baralhoProvisorio1 = [];
+  let baralhoProvisorio2 = [];
+
+  for (let i = 0; i < numDuplas; i++) {
+    baralhoProvisorio1[i] += todosPapagaios[i];
+    baralhoProvisorio2[i] += todosPapagaios[i];
+  }
+  novoBaralho = baralhoProvisorio1.concat(baralhoProvisorio2);
+
+  for (let i = 0; i < novoBaralho.length; i++) {
+    novoBaralho[i] = novoBaralho[i].replace("undefined", "");
+  }
+
+  novoBaralho.sort(aleatorizar);
+}
+
 function criarCartas() {
+  console.log("entrouaq");
+  const acharHtml = document.querySelector("main");
+
   for (let i = 0; i < qtdCartas; i++) {
-    const acharHtml = document.querySelector("main");
-    const cartaSimples = `<div class="carta"><img src="img/front.png" alt="front parrot" /></div>`;
+    const cartaSimples = `
+    <div class="carta"><img id=${i} src="img/${novoBaralho[i]}.gif" alt="front parrot" /></div>
+    `;
     acharHtml.innerHTML += cartaSimples;
   }
+}
+
+function aleatorizar() {
+  return Math.random() - 0.5;
 }
